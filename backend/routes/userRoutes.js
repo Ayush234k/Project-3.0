397f54f4d6c282c35e026fd5c5a3b3fa70c4c94a
Astha Tiwari <astha.tiwari0910@gmail.com>
2024-03-10 17:35:26 +0530

    changes in backend

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { createUser, loginUser, logoutUser, getAllUsers } from "../controllers/userController.js";
+import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
+
+const router = express.Router();
+
+router.route("/").post(createUser).get(authenticate, authorizeAdmin, getAllUsers);
+router.post("/auth", loginUser);
+router.post("/logout", logoutUser);
+
+export default router;
