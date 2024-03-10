716140663d3e2a07e78bc3f9439a972bca14e789
Astha Tiwari <astha.tiwari0910@gmail.com>
2024-03-10 20:40:59 +0530

    changes in backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,34 @@
 import express from "express";
-import { createUser, loginUser, logoutUser, getAllUsers } from "../controllers/userController.js";
+import {
+  createUser,
+  loginUser,
+  logoutUser,
+  getAllUsers,
+  getUser,
+  updateUser,
+  deleteUserById,
+  getUserById,
+  updateUserById
+} from "../controllers/userController.js";
 import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.route("/").post(createUser).get(authenticate, authorizeAdmin, getAllUsers);
+router
+  .route("/")
+  .post(createUser)
+  .get(authenticate, authorizeAdmin, getAllUsers);
 router.post("/auth", loginUser);
 router.post("/logout", logoutUser);
+router
+  .route("/profile")
+  .get(authenticate, getUser)
+  .put(authenticate, updateUser);
+
+router
+  .route("/:id")
+  .delete(authenticate, authorizeAdmin, deleteUserById)
+  .get(authenticate, authorizeAdmin, getUserById)
+  .put(authenticate, authorizeAdmin, updateUserById);
 
 export default router;
